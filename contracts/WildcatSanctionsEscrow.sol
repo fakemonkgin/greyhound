71de55c48db8c8f0391928a4fd700324c7f1fd4f
李晶 <lijing@lijings-MacBook-Air.local>
2024-09-20 18:34:34 +0800

    add more pattern

--- contracts/WildcatSanctionsEscrow.sol
+++ contracts/WildcatSanctionsEscrow.sol
@@ -1,15 +1,15 @@
 // // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
 
-// import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
-// import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-// import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-// import "../rewards/IMendiCToken.sol";
-// import "../comm/TransferHelper.sol";
-// import "../rewards/IRewardNew.sol";
-// import "./IFarm.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "../rewards/IMendiCToken.sol";
+import "../comm/TransferHelper.sol";
+import "../rewards/IRewardNew.sol";
+import "./IFarm.sol";
 
 contract BagfulMendiCompoundFarm is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable, IFarm {
     using SafeERC20 for IERC20;
